9f56c5e644455ad4af9783c50a738fbbb547e8cf
Seshchenko Dmitrii <DmitryW1Seschenko@ukr.net>
2020-10-07 21:30:43 +0300

    style user

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -14,21 +14,21 @@ const Users = (props) => {
 			{	id: 2,
 				followed: true,
 				name: 'Nastenka',
-				photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Volodymyr_Zelensky_Official_portrait_%28cropped%29.jpg/248px-Volodymyr_Zelensky_Official_portrait_%28cropped%29.jpg',
+				photoUrl: 'https://svirtus.cdnvideo.ru/SO9sxAqnAkR1tN1Kg6jXFhWvYMM=/0x0:371x371/800x0/filters:quality(100)/https://hb.bizmrg.com/cybersportru-media/d4/d4e61b79893464edf8fac84d5b25bcd6.jpg?m=6abf7ca4aedde894c0fb76aea5a89542',
 				status: 'I\'m Little girl',
-				locetion: {city: 'Sumy', country: 'Ukraine'}
+				locetion: {city: 'Kiev', country: 'Ukraine'}
 			},
 			{	id: 3,
 				followed: false,
-				name: 'Dima developer',
-				photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Volodymyr_Zelensky_Official_portrait_%28cropped%29.jpg/248px-Volodymyr_Zelensky_Official_portrait_%28cropped%29.jpg',
+				name: 'Dima BigBoss',
+				photoUrl: 'https://yt3.ggpht.com/a/AATXAJxegcKj7lnz8MCMgcAz-gpSgPs5u5yMvynOEWiL=s176-c-k-c0x00ffffff-no-rj-mo',
 				status: 'next lvl improve skills',
 				locetion: {city: 'Paris', country: 'Franch'}
 			},
 			{	id: 4,
 				followed: false,
 				name: 'Artem lowSkill',
-				photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Volodymyr_Zelensky_Official_portrait_%28cropped%29.jpg/248px-Volodymyr_Zelensky_Official_portrait_%28cropped%29.jpg',
+				photoUrl: 'https://abakan-news.ru/wp-content/uploads/2018/03/-e1520919096875.jpg',
 				status: 'All about this...',
 				locetion: {city: 'Sumy', country: 'Ukraine'}
 			},
